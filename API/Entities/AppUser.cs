8554d9ebf5570843f4a494cb500aca386d734335
Kaniram <kaniramkumawat91.com>
2023-05-30 16:18:58 +0530

    End of section 2

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace API.Entitied
+{
+    public class AppUser
+    {
+        [Key]
+        public int Id { get; set; }
+        public string UserName { get; set; }
+    }
+}
