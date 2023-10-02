3ab4a00dd14af45569938c800a1c96f11ad54484
KaniramKumawat <kaniramkumawat91@gmail.com>
2023-10-02 19:21:49 +0530

    End of video 91 Seeding data part two

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,4 +1,6 @@
 using System.ComponentModel.DataAnnotations;
+using API.Entities;
+using API.Extentions;
 
 namespace API.Entitied
 {
@@ -8,5 +10,20 @@ namespace API.Entitied
         public string UserName { get; set; }
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
+        public DateOnly DateOfBirth { get; set; }
+        public string KnowsAs { get; set; }
+        public DateTime Created { get; set; } = DateTime.UtcNow;
+        public DateTime LastActive { get; set; } = DateTime.UtcNow;
+        public string Gender { get; set; }
+        public string Introduction { get; set; }
+        public string LoolingFor { get; set; }
+        public string Interests { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public List<Photo> Photos { get; set; } = new();
+        public int GetAge()
+        {
+            return DateOfBirth.CalculateAge();
+        }
     }
 }
