62fdf63478d317073e7402e00eb294895b5c648f
KaniramKumawat <kaniramkumawat91@gmail.com>
2023-10-24 09:26:20 +0530

    End of section 9

--- client/src/app/_models/member.ts
+++ client/src/app/_models/member.ts
@@ -10,7 +10,7 @@ export interface Member {
     lastActive: string;
     gender: string;
     introduction: string;
-    loolingFor?: any;
+    lookingFor?: any;
     interests: string;
     city: string;
     country: string;
